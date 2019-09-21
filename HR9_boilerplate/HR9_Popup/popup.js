f90256495194cbf16bbe980915412853e4ef2d58
Jacob Lu <Jacob@jacobs-air.ncifcrf.gov>
2019-09-20 22:21:02 -0500

    Added boilerplate

---
+++ HR9_boilerplate/HR9_Popup/popup.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  var checkPageButton = document.getElementById('checkPage');
+
+  checkPageButton.addEventListener('click', function() {
+     window.open('https://hack.rice.edu/');
+  }, false);
+  
+}, false);

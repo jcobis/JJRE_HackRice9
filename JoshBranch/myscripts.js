62c30873a03edce9081dc21e38ee7ba60aa1890b
Josuha Engels <joshuaengels@yahoo.com>
2019-09-21 01:49:58 -0500

    Added ideas section to content file

--- JoshBranch/myscripts.js
+++ JoshBranch/myscripts.js
@@ -10,9 +10,15 @@ function highlightHandler(e) {
 
 document.onmouseup = highlightHandler;
 
+// Display wikipedia stuff above the top left corner of the text
 function doStuff(text) {
 	oRange = text.getRangeAt(0); //get the text range
 	oRect = oRange.getBoundingClientRect();
 	console.log(oRect);
 	console.log(text.toString())
 }
+
+
+// IDEAS
+// Might want disambugation support, link following through the popup, wikipedia interface
+// WIKIPEDIA INTEGRATION with article reading, maybe sharing screen or something

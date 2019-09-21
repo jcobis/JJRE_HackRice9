16c12a24af57f22dab9be55b8bd8c3437a03b80b
Josuha Engels <joshuaengels@yahoo.com>
2019-09-21 17:21:06 -0500

    Clicking on tooltip and outside tooltip working

--- JoshBranch/myscripts.js
+++ JoshBranch/myscripts.js
@@ -72,29 +72,49 @@ function tryToPopulateTip(title) {
 
 
 // Show tip when text selected
-document.onmouseup = selectionTip;
+// document.onmouseup = selectionTip;
 
-// Hide when click outside window
-// Source https://stackoverflow.com/questions/152975/how-do-i-detect-a-click-outside-an-element
-function hideOnClickOutside(element) {
+// If the window is not visisible and you release a click with highlighted text, it shows the window
+// If the window is visisible and you click outside the screen, it hides the window
+// Partial source https://stackoverflow.com/questions/152975/how-do-i-detect-a-click-outside-an-element
+function showAndHideListeners(element) {
+    
     const outsideClickListener = event => {
+        
+        // If click is outside box and box is onscreen,
         if (!element.contains(event.target) && isVisible(element)) { // or use: event.target.closest(selector) === null
           element.style.display = 'none';
-        };
+        }
+
     };
+
+    // Only update tip if selection exists and is outside the tooltip if there is any on display right now
+    const selectionShower = event => {
+        if (!isVisible(element) || !element.contains(event.target)) {
+            
+            // Gets the current selection
+            var sel = document.getSelection();
+            if (!sel.isCollapsed) {
+
+                // Set the tip location
+                setTipLocationToSelection();
+
+                // Try to populate the tip
+                tryToPopulateTip(sel.toString());
+
+            }
+        }
+    }
 
 
     document.addEventListener('click', outsideClickListener);
+    document.addEventListener('mouseup', selectionShower)
 };
+
 const isVisible = elem => !!elem && !!( elem.offsetWidth || elem.offsetHeight || elem.getClientRects().length ); // source (2018-03-11): https://github.com/jquery/jquery/blob/master/src/css/hiddenVisibleSelectors.js
-hideOnClickOutside(div);
+showAndHideListeners(div);
 
 // IDEAS
 // Might want disambugation support, link following through the popup, wikipedia interface
 // WIKIPEDIA INTEGRATION with article reading, maybe sharing screen or something
 // Preload and make calls for common words on the page (but make sure to impose 200 limits? Or for different API)
-
-
-// This was everything I needed and more (it's written all over your face) 20 seconds ago:
-//When you do this, your code will stop at this point until your request has been fully processed. All UI interactions would stop. Your page will look like it is frozen. These are not good things, so to reiterate what I mentioned in the previous section, you should keep your requests asynchronous.
-// does a rest response contain the call url?? Need to know this for matching requests to responses

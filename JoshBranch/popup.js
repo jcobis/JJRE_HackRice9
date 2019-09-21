43d80f73baf23343c40e514fc993eca5f6bb77ef
Jacob Lu <Jacob@jacobs-air.ncifcrf.gov>
2019-09-21 16:52:34 -0500

    Landing configuration

--- JoshBranch/popup.js
+++ JoshBranch/popup.js
@@ -1,21 +1,9 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+document.addEventListener('DOMContentLoaded', function() {
 
-'use strict';
+  var checkPageButton = document.getElementById('checkPage');
 
-let changeColor = document.getElementById('changeColor');
-
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
-
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
-};
+  checkPageButton.addEventListener('click', function() {
+     window.open('landing.html');
+  }, false);
+  
+}, false);

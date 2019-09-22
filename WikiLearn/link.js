0067490bb7d65dca1a3421a612d9f5a0325838dd
Elliot Butterworth <esb8@rice.edu>
2019-09-22 00:28:15 -0500

    styled links, fixed link titles

--- WikiLearn/link.js
+++ WikiLearn/link.js
@@ -7,9 +7,9 @@ function hrefExtracter(htmlString) {
 
     currentPos = start;
     var substring = htmlString.substring(start, start + 20);
-    
+
     if (substring == "<a rel=\"mw:WikiLink\"") {
-   
+
       while (htmlString.substring(currentPos - 4, currentPos) != "</a>"){
         currentPos += 1;
       }
@@ -17,16 +17,14 @@ function hrefExtracter(htmlString) {
 
       linkString = htmlString.substring(start, end);
 
-
-
       var el = document.createElement( 'a' );
       el.innerHTML = linkString;
       var wikiTitle = el.childNodes[0].title;
       var text = el.childNodes[0].innerText;
-      var replacementForText = "<button class='link' id=" + encodeURIComponent(text.trim()) + ">" + text + "</button>";
+      var replacementForText = "<button class='link' id=" + encodeURIComponent(text.trim()) + " data-wiki=" + encodeURIComponent(wikiTitle.trim()) + ">" + text + "</button>";
       array.push(text);
       array.push(replacementForText);
-    } 
+    }
     start = currentPos + 1;
   }
   return array;
